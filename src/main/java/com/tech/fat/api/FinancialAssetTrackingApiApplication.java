0f9eb783d5c17e4dcbb2c7270df722f2d1e8363a
zhouxuh <18181680+zhouxuh@users.noreply.github.com>
2022-11-20 00:40:38 -0500

    Initial commit

---
+++ src/main/java/com/tech/fat/api/FinancialAssetTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.tech.fat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialAssetTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialAssetTrackingApiApplication.class, args);
+	}
+
+}
